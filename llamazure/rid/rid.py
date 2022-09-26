27f9561dcd6d8c7bfd07babd6f2d004c03a05bc4
Daniel Goldman <danielgoldman4@gmail.com>
2022-09-25 23:34:23 -0400

    fill out parse function

--- llamazure/rid/rid.py
+++ llamazure/rid/rid.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
 import abc
 from dataclasses import dataclass
+from typing import Optional, Union
+
 
 class AzObj(abc.ABC):
 	"""An Azure object"""
@@ -23,4 +27,32 @@ class Resource(AzObj):
 	provider: str
 	res_type: str
 	name: str
-	parent: Union[ResourceGroup, Resource]
+	rg: ResourceGroup
+	parent: Optional["Resource"]
+
+
+def parse(rid: str) -> Optional[AzObj]:
+	parts = iter(rid.lower().split("/"))
+
+	out = None
+	try:
+		if next(parts) == "subscription":
+			out = Subscription(next(parts))
+		else:
+			return None
+
+		if next(parts) == "resourcegroups":
+			out = rg = ResourceGroup(next(parts), out)
+		else:
+			return out
+
+		parent = None
+		while True:
+			if next(parts) == "providers":
+				provider = next(parts)
+				res_type = next(parts)
+				name = next(parts)
+				parent = Resource(provider, res_type, name, parent=parent, rg=rg)
+
+	except StopIteration:
+		return out
