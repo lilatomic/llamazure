0b5c3c3683745daf10ab88efd17ea035db12f3c7
Daniel Goldman <danielgoldman4@gmail.com>
2022-09-26 00:05:16 -0400

    use custom generator to get valid az resources

--- llamazure/rid/rid_test.py
+++ llamazure/rid/rid_test.py
@@ -1,9 +1,15 @@
+import string
 from uuid import UUID
 
 from hypothesis import given
-from hypothesis.strategies import text, uuids
+from hypothesis.strategies import text, uuids, builds
 
 from llamazure.rid.rid import parse, Subscription, ResourceGroup
+
+
+az_alnum = text(alphabet=list(string.ascii_letters + string.digits))
+
+st_subscription = builds(lambda u: Subscription(str(u)), uuids())
 
 
 class TestRIDTypes:
@@ -12,6 +18,6 @@ class TestRIDTypes:
 	def test_subscription(self, u: UUID):
 		assert parse(f"subscription/{u}") == Subscription(str(u))
 
-	@given(uuids(), text())
-	def test_resource_group(self, u: UUID, rg: str):
-		assert parse(f"subscription/{u}/resourceGroups/{rg}") == ResourceGroup(rg.lower(), Subscription(str(u)))
+	@given(st_subscription, az_alnum)
+	def test_resource_group(self, sub: Subscription, rg: str):
+		assert parse(f"subscription/{sub.uuid}/resourceGroups/{rg}") == ResourceGroup(rg.lower(), sub)
