ef0c850421505ff39f310550f5d6035436350c45
Daniel Goldman <danielgoldman4@gmail.com>
2022-09-26 00:10:00 -0400

    more custom strategies

--- llamazure/rid/rid_test.py
+++ llamazure/rid/rid_test.py
@@ -9,8 +9,11 @@ from llamazure.rid.rid import parse, Subscription, ResourceGroup
 
 az_alnum = text(alphabet=list(string.ascii_letters + string.digits))
 
+az_alnum_lower = text(alphabet=list(string.ascii_lowercase + string.digits))
+
 st_subscription = builds(lambda u: Subscription(str(u)), uuids())
 
+st_rg = builds(lambda sub, name: ResourceGroup(name, sub), st_subscription, az_alnum_lower)
 
 class TestRIDTypes:
 
@@ -21,3 +24,7 @@ class TestRIDTypes:
 	@given(st_subscription, az_alnum)
 	def test_resource_group(self, sub: Subscription, rg: str):
 		assert parse(f"subscription/{sub.uuid}/resourceGroups/{rg}") == ResourceGroup(rg.lower(), sub)
+
+	@given(st_rg)
+	def test_resource_group_constructed(self, rg: ResourceGroup):
+		assert parse(f"subscription/{rg.subscription.uuid}/resourceGroups/{rg.name}") == rg
