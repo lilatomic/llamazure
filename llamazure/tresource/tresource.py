25bbe38343356dad280da459bd2c614bc9f3cd44
Daniel Goldman <danielgoldman4@gmail.com>
2022-10-22 21:17:05 -0400

    add Resources and SubResources and complex resources to Tresource

--- llamazure/tresource/tresource.py
+++ llamazure/tresource/tresource.py
@@ -1,9 +1,9 @@
 """Build a tree of Azure resources"""
 from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import DefaultDict, Dict
+from typing import DefaultDict, Dict, List
 
-from llamazure.rid.rid import AzObj, ResourceGroup, Subscription
+from llamazure.rid.rid import AzObj, ResourceGroup, Subscription, Resource, SubResource
 
 recursive_default_dict = lambda: defaultdict(recursive_default_dict)
 
@@ -18,8 +18,26 @@ class Tresource:
 		"""Add a resource to the tree"""
 		if isinstance(obj, Subscription):
 			self.resources[obj].update()
-		if isinstance(obj, ResourceGroup):
+		elif isinstance(obj, ResourceGroup):
 			self.resources[obj.sub][obj].update()
+		elif isinstance(obj, Resource) or isinstance(obj, SubResource):
+			# Adding a Resource or SubResource requires us to add all parents
+			# The llamazure.rid makes it very easy to trace backwards,
+			# and that adds complexity here
+			inv_path = [obj]
+			r = obj
+			while r.parent:
+				inv_path.append(r.parent)
+				r = r.parent
+
+			def mut_recurse(d, parts):
+				if parts:
+					part = parts.pop()
+					mut_recurse(d[part], parts)
+
+			mut_recurse(self.resources, inv_path + [obj.rg, obj.sub])
+
+
 
 	@property
 	def subs(self):
@@ -27,12 +45,38 @@ class Tresource:
 		return list(self.resources.keys())
 
 	@property
-	def rgs(self):
+	def rgs(self) -> Dict[Subscription, List[ResourceGroup]]:
 		"""
 		Get RGs nested by subscription
 		"""
 		return {sub: list(rgs.keys()) for sub, rgs in self.resources.items()}
 
-	@property
-	def rgs_flat(self):
+	def rgs_flat(self) -> List[ResourceGroup]:
+		"""
+		Get RGs as a flat list
+		"""
 		return [rg for rgs in self.resources.values() for rg in rgs]
+
+	@property
+	def res(self):
+		return self.resources
+
+	def res_flat(self):
+		"""
+		Return all resources flattened into a list,
+		including resources that were implicitly added as a parent of another resource
+		"""
+		out = []
+
+		def recurse_resources(res, children):
+			out.append(res)
+			if children:
+				for child, subchildren in children.items():
+					recurse_resources(child, subchildren)
+
+		for rgs in self.resources.values():
+			for ress in rgs.values():
+				for res, children in ress.items():
+					recurse_resources(res, children)
+
+		return out
--- llamazure/tresource/tresource_test.py
+++ llamazure/tresource/tresource_test.py
@@ -1,11 +1,11 @@
 """Test Tresource"""
-from typing import List
+from typing import List, Union
 
 from hypothesis import given
 from hypothesis.strategies import lists
 
-from llamazure.rid.rid import ResourceGroup
-from llamazure.rid.rid_test import st_rg, st_subscription
+from llamazure.rid.rid import Resource, ResourceGroup, SubResource
+from llamazure.rid.rid_test import st_resource_base, st_rg, st_subscription, st_resource_complex
 from llamazure.tresource.tresource import Tresource
 
 
@@ -24,7 +24,7 @@ class TestBuildTree:
 		assert len(set(tree.subs)) == len(subs)
 
 	@given(lists(st_rg))
-	def test_build_rg(self, rgs: List[ResourceGroup]):
+	def test_build_rgs(self, rgs: List[ResourceGroup]):
 		"""Test adding only RGs"""
 		tree = Tresource()
 
@@ -35,4 +35,45 @@ class TestBuildTree:
 			tree.add(rg)
 
 		assert subs == set(tree.subs)
-		assert set(rgs) == set(tree.rgs_flat)
+		assert set(rgs) == set(tree.rgs_flat())
+
+	@given(lists(st_resource_base))
+	def test_build_simple_resources(self, ress: List[Resource]):
+		tree = Tresource()
+
+		subs = set()
+		rgs = set()
+
+		for res in ress:
+			subs.add(res.sub)
+			rgs.add(res.rg)
+			tree.add(res)
+
+		assert subs == set(tree.subs)
+		assert rgs == set(tree.rgs_flat())
+		# since there is no nesting, there are no implicit resources, and this comparison is valid
+		assert set(ress) == set(tree.res_flat())
+
+	@given(lists(st_resource_complex))
+	def test_build_complex_resources(self, ress: List[Union[Resource, SubResource]]):
+		tree = Tresource()
+
+		subs = set()
+		rgs = set()
+		resources = set()
+
+		def recurse_register(resource):
+			resources.add(resource)
+			if resource.parent:
+				recurse_register(resource.parent)
+
+		for res in ress:
+			subs.add(res.sub)
+			rgs.add(res.rg)
+			recurse_register(res)
+			tree.add(res)
+
+		assert subs == set(tree.subs)
+		assert rgs == set(tree.rgs_flat())
+		# since there is nesting, there are implicit resources, and there will be more
+		assert resources == set(tree.res_flat())
