a7d0e773a979c592d356953c05c203172236e345
Daniel Goldman <danielgoldman4@gmail.com>
2022-12-15 01:44:47 -0500

    use common tests

--- llamazure/tresource/mp_test.py
+++ llamazure/tresource/mp_test.py
@@ -1,69 +1,32 @@
 """Test TresourceMP"""
-from typing import List, Type
-
-from hypothesis import given
-from hypothesis.strategies import lists
+from typing import Type
 
 from llamazure.rid import conv, rid
-from llamazure.rid.conftest import st_resource_base, st_rg, st_subscription
-from llamazure.rid.conv import rid2mp
-from llamazure.rid.mp import AzObj, Path, Resource
+from llamazure.rid.mp import AzObj, Path
 from llamazure.tresource.conftest import ABCTestBuildTree
+from llamazure.tresource.itresource import AzObjT, ObjReprT
 from llamazure.tresource.mp import TresourceMP, TresourceMPData
 
 
-class TestBuildTree:
+class TestBuildTreeMP(ABCTestBuildTree[AzObj, Path]):
 	"""Test that building the tree is correct and seamless"""
 
-	@given(lists(st_subscription))
-	def test_build_subscriptions(self, subs):
-		subs = [rid2mp(sub) for sub in subs]
-		tree = TresourceMP()
+	@property
+	def clz(self) -> Type:
+		return TresourceMP
 
-		for sub in subs:
-			tree.add(sub)
+	def conv(self, obj: rid.AzObj) -> AzObjT:
+		return conv.rid2mp(obj)
 
-		assert tree.subs() == set(sub.path for sub in subs)
+	def recover(self, obj_repr: ObjReprT) -> rid.AzObj:
+		return rid.parse(obj_repr)
 
-	@given(lists(st_rg))
-	def test_build_rgs(self, rgs):
-		rgs = [rid2mp(rg) for rg in rgs]
-		tree = TresourceMP()
-
-		subs = set()
-		for rg in rgs:
-			subs.add(rg.sub)
-			tree.add(rg)
-
-		assert subs == tree.subs()
-		assert set(rg.path for rg in rgs) == set(tree.rgs_flat())
-
-	@given(lists(st_resource_base))
-	def test_build_simple_resource(self, ress_rid: List[rid.Resource]):
-		ress: List[Resource] = []
-		for res_rid in ress_rid:
-			converted = rid2mp(res_rid)
-			assert isinstance(converted, Resource)
-			ress.append(converted)
-		tree = TresourceMP()
-
-		subs = set()
-		rgs = set()
-
-		for res in ress:
-			subs.add(res.sub)
-			if res.rg:
-				rgs.add(res.rg)
-			tree.add(res)
-
-		assert subs == tree.subs()
-		assert rgs == set(tree.rgs_flat())
-
-		# since there is no nesting, there are no implicit resources, and this comparison is valid
-		assert set(x.path for x in ress) == set(tree.res_flat())
+	@property
+	def recurse_implicit(self) -> bool:
+		return False
 
 
-class TestBuildDataTree(ABCTestBuildTree[AzObj, Path]):
+class TestBuildTreeMPData(ABCTestBuildTree[AzObj, Path]):
 	"""Test building a TresourceData"""
 
 	@property
--- llamazure/tresource/tresource_test.py
+++ llamazure/tresource/tresource_test.py
@@ -5,81 +5,29 @@ from hypothesis import given
 from hypothesis.strategies import lists
 
 from llamazure.rid import rid
-from llamazure.rid.conftest import st_resource_any, st_resource_base, st_resource_complex, st_rg, st_subscription
-from llamazure.rid.rid import AzObj, Resource, ResourceGroup, SubResource, parse_chain, serialise
+from llamazure.rid.conftest import st_resource_any, st_resource_complex
+from llamazure.rid.rid import AzObj, Resource, SubResource, parse_chain, serialise
 from llamazure.tresource.conftest import ABCTestBuildTree
+from llamazure.tresource.itresource import AzObjT, ITresource, ObjReprT
 from llamazure.tresource.tresource import Node, Tresource, TresourceData
 
 
-class TestBuildTree:
+class TestBuildTree(ABCTestBuildTree[AzObj, AzObj]):
 	"""Test that building a tree is correct and seamless"""
 
-	@given(lists(st_subscription))
-	def test_build_subscriptions(self, subs):
-		"""Test adding only subscriptions"""
-		tree = Tresource()
+	@property
+	def clz(self) -> Type[ITresource]:
+		return Tresource
 
-		for sub in subs:
-			tree.add(sub)
+	def conv(self, obj: rid.AzObj) -> AzObjT:
+		return obj
 
-		assert len(tree.subs()) == len(subs)
+	def recover(self, obj_repr: ObjReprT) -> rid.AzObj:
+		return obj_repr
 
-	@given(lists(st_rg))
-	def test_build_rgs(self, rgs: List[ResourceGroup]):
-		"""Test adding only RGs"""
-		tree = Tresource()
-
-		subs = set()
-
-		for rg in rgs:
-			subs.add(rg.sub)
-			tree.add(rg)
-
-		assert subs == tree.subs()
-		assert set(rgs) == set(tree.rgs_flat())
-
-	@given(lists(st_resource_base))
-	def test_build_simple_resources(self, ress: List[Resource]):
-		tree = Tresource()
-
-		subs = set()
-		rgs = set()
-
-		for res in ress:
-			subs.add(res.sub)
-			if res.rg:
-				rgs.add(res.rg)
-			tree.add(res)
-
-		assert subs == tree.subs()
-		assert rgs == set(tree.rgs_flat())
-		# since there is no nesting, there are no implicit resources, and this comparison is valid
-		assert set(ress) == set(tree.res_flat())
-
-	@given(lists(st_resource_complex))
-	def test_build_complex_resources(self, ress: List[Union[Resource, SubResource]]):
-		tree = Tresource()
-
-		subs = set()
-		rgs = set()
-		resources = set()
-
-		def recurse_register(resource):
-			resources.add(resource)
-			if resource.parent:
-				recurse_register(resource.parent)
-
-		for res in ress:
-			subs.add(res.sub)
-			if res.rg:
-				rgs.add(res.rg)
-			recurse_register(res)
-			tree.add(res)
-
-		assert subs == tree.subs()
-		assert rgs == set(tree.rgs_flat())
-		# since there is nesting, there are implicit resources, and there will be more
-		assert resources == set(tree.res_flat())
+	@property
+	def recurse_implicit(self) -> bool:
+		return True
 
 
 class TestBuildTreeFromChain:
