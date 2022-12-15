7ed208d8f91b0c446f30849ef54c333be61db30e
Daniel Goldman <danielgoldman4@gmail.com>
2022-12-14 20:55:22 -0500

    rebuild ITresource as ABC

--- llamazure/tresource/itresource.py
+++ llamazure/tresource/itresource.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Generic, List, Protocol, Set, TypeVar
 
 AzObjT = TypeVar("AzObjT")
@@ -5,17 +6,20 @@ DataT = TypeVar("DataT")
 ObjReprT = TypeVar("ObjReprT")
 
 
-class ITresource(Protocol[AzObjT, ObjReprT]):
+class ITresource(Generic[AzObjT, ObjReprT], ABC):
 	"""Generic interface for all Tresources"""
 
+	@abstractmethod
 	def subs(self) -> Set[ObjReprT]:
 		"""Return all subscriptions that contain resources in this tresource"""
 		...
 
+	@abstractmethod
 	def rgs_flat(self) -> Set[ObjReprT]:
 		"""Return all resource groups that contain resources in this tresource"""
 		...
 
+	@abstractmethod
 	def res_flat(self) -> Set[ObjReprT]:
 		"""Resturn all explicit resources in this tresource"""
 		...
@@ -29,8 +33,9 @@ class INode(Protocol[AzObjT, DataT]):
 NodeT = TypeVar("NodeT", bound=INode)
 
 
-class ITresourceData(ITresource[AzObjT, ObjReprT], Protocol[AzObjT, NodeT, ObjReprT]):
+class ITresourceData(Generic[AzObjT, NodeT, ObjReprT], ITresource[AzObjT, ObjReprT]):
 	"""Generic interface for a TresourceData"""
 
+	@abstractmethod
 	def set_data(self, obj: AzObjT, data: DataT):
 		...
