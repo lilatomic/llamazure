47500c675f07019485c188f31b99be9b05d898c5
Daniel Goldman <danielgoldman4@gmail.com>
2023-03-26 14:32:10 -0400

    init ms graph

---
+++ llamazure/rbac/msgraph.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import requests
+
+
+class Graph:
+	"""
+	Access the Microsoft Graph
+	"""
+
+	def __init__(self, token):
+		self.token = token
+
+	@classmethod
+	def from_credential(cls, credential) -> Graph:
+		"""Create from an Azure credential"""
+		token = credential.get_token("https://graph.microsoft.com/")
+		return cls(token)
+
+	def q(self, q: str):
+		"""Make a graph query"""
+		return self._exec_query(q)
+
+	def _exec_query(self, req):
+		raw = requests.get(
+			f"https://graph.microsoft.com/v1.0/{req}",
+			headers={"Authorization": f"Bearer {self.token.token}"}
+		)
+		return raw
---
+++ llamazure/rbac/readme.md
@@ -0,0 +1,3 @@
+# llamazure.rbac : Azure roles, users, and assignments
+
+The `llamazure.rbac` package provides a helpful interface to Azure RBAC.
