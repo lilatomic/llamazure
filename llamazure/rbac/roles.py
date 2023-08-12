9bab47c1d59b2a3ebbff4f616a8b366c7fa8a7c9
Daniel Goldman <danielgoldman4@gmail.com>
2023-08-12 12:23:27 -0400

    transclude some REST methods
    Maybe I'll write a OpenAPI generator

--- llamazure/rbac/roles.py
+++ llamazure/rbac/roles.py
@@ -1,7 +1,9 @@
 """Azure Role Definitions and Assignments"""
 from __future__ import annotations
 
-from typing import Dict, Sequence
+from typing import Dict, List
+
+from pydantic import BaseModel
 
 from llamazure.azrest.azrest import AzRest
 
@@ -9,16 +11,51 @@ RoleDefT = Dict
 RoleAsnT = Dict
 
 
-class RoleDefs:
+class Permission(BaseModel):
+	actions: List[str]
+	notActions: List[str]
+	dataActions: List[str]
+	notDataActions: List[str]
+
+
+class RoleDefinition(BaseModel):
+	class Properties(BaseModel):
+		roleName: str
+		description: str
+		permissions: List[Permission]
+		assignableScopes: List[str]
+
+	rid: str
+	name: str
+	type_: str
+	properties: Properties
+
+
+class RoleDefinitions:
 	"""Interact with Azure RoleDefinitions"""
 
 	provider_type = "Microsoft.Authorization/roleDefinitions"
-	provider_slug = "/subscriptions/{subscription_id}/providers/Microsoft.Authorization/roleDefinitions"
+	apiv = "2022-04-01"
 
 	def __init__(self, azrest: AzRest):
 		self.azrest = azrest
 
-	def list_at_subscription(self, subscription_id) -> Sequence[RoleDefT]:
-		"""Get roles at a subscription"""
-		url = self.provider_slug.format(subscription_id=subscription_id)
-		return self.azrest.get(url)
+	def Delete(self, scope: str, roleDefinitionId: str):
+		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
+		return self.azrest.delete(slug, self.apiv)
+
+	def Get(self, scope: str, roleDefinitionId: str):
+		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
+		return self.azrest.get(slug, self.apiv)
+
+	def GetById(self, roleId: str):
+		slug = f"/{roleId}?disambiguation_dummy"
+		return self.azrest.get(slug, self.apiv)
+
+	def CreateOrUpdate(self, scope, roleDefinitionId: str, roleDefinition: RoleDefinition):
+		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
+		return self.azrest.put(slug, self.apiv, roleDefinition)
+
+	def List(self, scope: str):
+		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions"
+		return self.azrest.get(slug, self.apiv)
