6b468c6ee7ecb0bb69e083a2e23aa93f32a52c6c
Daniel Goldman <danielgoldman4@gmail.com>
2023-08-12 14:43:14 -0400

    start building helper layer

--- llamazure/rbac/roles.py
+++ llamazure/rbac/roles.py
@@ -1,7 +1,9 @@
 """Azure Role Definitions and Assignments"""
 from __future__ import annotations
 
+from functools import cached_property
 from typing import Dict, List
+from uuid import uuid4
 
 from pydantic import BaseModel, Field
 
@@ -31,7 +33,7 @@ class RoleDefinition(BaseModel):
 	properties: Properties
 
 
-class RoleDefinitions:
+class AzRoleDefinitions:
 	"""Interact with Azure RoleDefinitions"""
 
 	provider_type = "Microsoft.Authorization/roleDefinitions"
@@ -41,11 +43,11 @@ class RoleDefinitions:
 		self.azrest = azrest
 
 	def Delete(self, scope: str, roleDefinitionId: str):
-		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
+		slug = f"{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
 		return self.azrest.delete(slug, self.apiv)
 
 	def Get(self, scope: str, roleDefinitionId: str):
-		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
+		slug = f"{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
 		ret = self.azrest.get(slug, self.apiv)
 		return RoleDefinition(**ret)
 
@@ -54,10 +56,42 @@ class RoleDefinitions:
 		ret = self.azrest.get(slug, self.apiv)
 		return RoleDefinition(**ret)
 
-	def CreateOrUpdate(self, scope, roleDefinitionId: str, roleDefinition: RoleDefinition):
-		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
-		return self.azrest.put(slug, self.apiv, roleDefinition)
+	def CreateOrUpdate(self, scope, roleDefinitionId: str, roleDefinition: RoleDefinition) -> RoleDefinition:
+		slug = f"{scope}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId}"
+		ret = self.azrest.put(slug, self.apiv, roleDefinition)
+		return RoleDefinition(**ret)
 
-	def List(self, scope: str):
-		slug = f"/{scope}/providers/Microsoft.Authorization/roleDefinitions"
-		return self.azrest.get(slug, self.apiv)
+	def List(self, scope: str) -> List[RoleDefinition]:
+		slug = f"{scope}/providers/Microsoft.Authorization/roleDefinitions"
+		ret = self.azrest.get(slug, self.apiv)
+		return [RoleDefinition(**e) for e in ret["value"]]
+
+
+class RoleDefinitions(AzRoleDefinitions):
+	@cached_property
+	def cached(self) -> List[RoleDefinition]:
+		return self.List("")
+
+	@cached_property
+	def by_name(self):
+		return {e.properties.roleName: e for e in self.cached}
+
+	def get_by_name(self, name: str) -> RoleDefinition:
+		return self.by_name[name]
+
+	def put(self, role: RoleDefinition.Properties, scope: str = "/") -> RoleDefinition:
+		existing_role: RoleDefinition = self.by_name.get(role.roleName, None)
+		if existing_role:
+			target_role = existing_role.model_copy(update={"properties": role})
+			# copy assignable scopes
+			if not role.assignableScopes:
+				target_role.properties.assignableScopes = existing_role.properties.assignableScopes
+		else:
+			name = str(uuid4())
+			target_role = RoleDefinition(name=name, properties=role)
+
+		# ensure that the role definition scope is in the assignable scopes
+		if scope not in target_role.properties.assignableScopes:
+			target_role.properties.assignableScopes.append(scope)
+
+		return self.CreateOrUpdate(scope, target_role.name, target_role)
