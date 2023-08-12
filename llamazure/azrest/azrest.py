460d515904e15284f88afd904084d13b441e442b
Daniel Goldman <danielgoldman4@gmail.com>
2023-08-12 14:37:54 -0400

    raise error if not ok

--- llamazure/azrest/azrest.py
+++ llamazure/azrest/azrest.py
@@ -7,6 +7,11 @@ import requests
 from pydantic import BaseModel
 
 
+class AzureError(Exception):
+	def __init__(self, json):
+		self.json = json
+
+
 class AzRest:
 	def __init__(self, token, session: requests.Session, base_url: str = "https://management.azure.com/"):
 		self.token = token
@@ -24,7 +29,11 @@ class AzRest:
 	def call(self, req: requests.Request) -> Any:
 		"""Make the request to Azure"""
 		req.headers["Authorization"] = f"Bearer {self.token.token}"  # TODO: push down into self.session
-		return self.session.send(req.prepare()).json()  # TODO: write yet another fun interface to Azure
+		res = self.session.send(req.prepare())  # TODO: write yet another fun interface to Azure
+		if not res.ok:
+			raise AzureError(res.json())
+
+		return res.json()
 
 	def get(self, slug: str, apiv: str) -> Any:
 		return self.call(requests.Request("GET", urljoin(self.base_url, slug), params={"api-version": apiv}))
