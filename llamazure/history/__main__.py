b9cfee9ccf60bfac4837070ca1cc172ca17f6c47
Daniel Goldman <danielgoldman4@gmail.com>
2023-12-31 12:36:57 -0500

    load everything into a tresource

--- llamazure/history/__main__.py
+++ llamazure/history/__main__.py
@@ -1,9 +1,39 @@
+import json
 import os
+from dataclasses import is_dataclass, asdict
 
+from azure.identity import DefaultAzureCredential
+
+from llamazure.azgraph.azgraph import Graph
 from llamazure.history.data import TSDB, DB
+from llamazure.rid import mp
+from llamazure.tresource.mp import TresourceMPData, MPData
+
+
+class DataclassEncoder(json.JSONEncoder):
+	def default(self, obj):
+		if is_dataclass(obj):
+			return asdict(obj)
+		return super().default(obj)
+
+
+def reformat(resources):
+	for r in resources:
+		path, azobj = mp.parse(r["id"])
+		mpdata = MPData(azobj, r)
+		yield path, mpdata
+
 
 if __name__ == "__main__":
 	tsdb = TSDB(connstr=os.environ.get("connstr"))
 	db = DB(tsdb)
 
 	db.create_tables()
+
+	g = Graph.from_credential(DefaultAzureCredential())
+	resources = g.q("Resources")
+
+	tree = TresourceMPData()
+	tree.add_many(reformat(resources))
+
+	print(json.dumps(tree, cls=DataclassEncoder, indent=2))
