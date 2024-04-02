05046bae4c9f8c44372d956cdd91fe435c3d8ec7
Daniel Goldman <danielgoldman4@gmail.com>
2024-04-02 00:33:01 -0400

    only load test secrets if necessary

--- llamazure/test/credentials.py
+++ llamazure/test/credentials.py
@@ -6,16 +6,17 @@ from azure.identity import AzureCliCredential, ClientSecretCredential, Credentia
 
 def load_credentials():
 	"""Load credentials for a Service Principal"""
-	secrets = os.environ.get("integration_test_secrets")
-	if not secrets:
-		with open("cicd/secrets.yml", mode="r", encoding="utf-8") as f:
-			secrets = f.read()
-	secrets = yaml.safe_load(secrets)
 	try:
 		cli_credential = AzureCliCredential()
 		cli_credential.get_token("https://management.azure.com//.default")
 		return cli_credential
 	except CredentialUnavailableError:
+		secrets = os.environ.get("integration_test_secrets")
+		if not secrets:
+			with open("cicd/secrets.yml", mode="r", encoding="utf-8") as f:
+				secrets = f.read()
+		secrets = yaml.safe_load(secrets)
+
 		client = secrets["auth"]
 
 		credential = ClientSecretCredential(tenant_id=client["tenant"], client_id=client["appId"], client_secret=client["password"])
