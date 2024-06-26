e8c83ce37baf7d50a52db35539a65ca64c9064ed
Daniel Goldman <danielgoldman4@gmail.com>
2024-06-26 00:05:13 -0400

    fix: use newer name

--- llamazure/azrest/openapi.py
+++ llamazure/azrest/openapi.py
@@ -46,7 +46,7 @@ class OARef(BaseModel):
 	description: Optional[str] = None
 
 	class Config:
-		allow_population_by_field_name = True
+		populate_by_name = True
 
 	@property
 	def name(self) -> str:
