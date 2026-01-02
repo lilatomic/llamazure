656011c623833227835d7d7c1716f00cb26c457a
Daniel Goldman <danielgoldman4@gmail.com>
2026-01-01 21:25:02 -0500

    fix: subresource helpers pass parents correctly

--- llamazure/rid/rid.py
+++ llamazure/rid/rid.py
@@ -22,13 +22,6 @@ class AzObj(abc.ABC):
 		"""
 		...
 
-	def resource(self, provider: str, res_type: str, name: str) -> Resource:
-		return Resource(provider, res_type, name, rg=self.rg, sub=self.sub, parent=self)
-
-	def subresource(self, res_type: str, name: str) -> SubResource:
-		return SubResource(res_type, name, rg=self.rg, sub=self.sub, parent=self)
-
-
 
 @dataclass(frozen=True)
 class Subscription(AzObj):
@@ -42,6 +35,12 @@ class Subscription(AzObj):
 	def rg(self, name: str) -> ResourceGroup:
 		return ResourceGroup(name, self)
 
+	def resource(self, provider: str, res_type: str, name: str) -> Resource:
+		return Resource(provider, res_type, name, rg=None, sub=self)
+
+	def subresource(self, res_type: str, name: str) -> SubResource:
+		return SubResource(res_type, name, rg=None, sub=self)
+
 
 @dataclass(frozen=True)
 class ResourceGroup(AzObj):
@@ -52,6 +51,12 @@ class ResourceGroup(AzObj):
 
 	def slug(self) -> str:
 		return f"/resourcegroups/{self.name}"
+
+	def resource(self, provider: str, res_type: str, name: str) -> Resource:
+		return Resource(provider, res_type, name, rg=self, sub=self.sub)
+
+	def subresource(self, res_type: str, name: str) -> SubResource:
+		return SubResource(res_type, name, rg=self, sub=self.sub)
 
 
 @dataclass(frozen=True)
@@ -68,6 +73,12 @@ class Resource(AzObj):
 	def slug(self) -> str:
 		return f"/providers/{self.provider}/{self.res_type}/{self.name}"
 
+	def resource(self, provider: str, res_type: str, name: str) -> Resource:
+		return Resource(provider, res_type, name, rg=self.rg, sub=self.sub, parent=self)
+
+	def subresource(self, res_type: str, name: str) -> SubResource:
+		return SubResource(res_type, name, rg=self.rg, sub=self.sub, parent=self)
+
 
 @dataclass(frozen=True)
 class SubResource(AzObj):
@@ -81,6 +92,12 @@ class SubResource(AzObj):
 
 	def slug(self) -> str:
 		return f"/{self.res_type}/{self.name}"
+
+	def resource(self, provider: str, res_type: str, name: str) -> Resource:
+		return Resource(provider, res_type, name, rg=self.rg, sub=self.sub, parent=self)
+
+	def subresource(self, res_type: str, name: str) -> SubResource:
+		return SubResource(res_type, name, rg=self.rg, sub=self.sub, parent=self)
 
 
 def parse(rid: str) -> AzObj:
