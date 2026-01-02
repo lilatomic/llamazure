6967ecd438b71c9bed3e2182d9b1e40fec7f3a0a
Daniel Goldman <danielgoldman4@gmail.com>
2026-01-01 21:36:58 -0500

    feature: add default imports

---
+++ llamazure/azrest/__init__.py
@@ -0,0 +1,4 @@
+from llamazure.azrest.azrest import AzOps, AzRest
+from llamazure.azrest.models import AzList, AzureError, BatchReq, Req
+
+__all__ = ["AzRest", "AzOps", "Req", "BatchReq", "AzureError", "AzList"]
---
+++ llamazure/msgraph/__init__.py
@@ -0,0 +1,4 @@
+from llamazure.msgraph.models import MicrosoftGraphException, QueryOpts, Req, Res, ResErr, ResMaybe
+from llamazure.msgraph.msgraph import Graph
+
+__all__ = ["Graph", "Req", "Res", "ResErr", "MicrosoftGraphException", "QueryOpts", "ResMaybe"]
